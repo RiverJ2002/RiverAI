25e76d44e2ab463737ac3d0960ce8bd321b88e1b
River <jahandealireza@yahoo.com>
2024-12-14 21:43:40 +0330

    RouterChanged

--- src/App.jsx
+++ src/App.jsx
@@ -7,36 +7,38 @@ import SignInPage from './components/SignInPage'
 import { createBrowserRouter , RouterProvider} from 'react-router-dom'
 import ProfilePage from './components/ProfilePage';
 
-//Hello world
 
-var router = createBrowserRouter([{
-  path: '/',
-  element: <NormalLogin/>
-},
-{
-  path: '/HistoryPage',
-  element: <HistoryPage/>,
-  errorElement: <div>404 not found</div>
-},
-{
-  path: '/HistoryPage/ChatPage',
-  element: <ChatPage/>,
-  errorElement: <div>404 not found</div>
-},
 
-{
-  path: '/HistoryPage/ProfilePage',
-  element: <ProfilePage/>,
-  errorElement: <div>404 not found</div>
-},
+//creating the  router browswer. This was suggested somewhere. 
+var router = createBrowserRouter([
+  {
+    path: '/',
+    element: <NormalLogin/>
+  },
+  {
+    path: '/HistoryPage',
+    element: <HistoryPage/>,
+    errorElement: <div>404 not found</div>
+  },
+  {
+    path: '/HistoryPage/ChatPage',
+    element: <ChatPage/>,
+    errorElement: <div>404 not found</div>
+  },
+  {
+    path: '/HistoryPage/ProfilePage',
+    element: <ProfilePage/>,
+    errorElement: <div>404 not found</div>
+  },
+  {
+    path: '/SignInPage',
+    element: <SignInPage/>,
+    errorElement: <div>404 not found</div>
+  }
+], {
+  basename: "/RiverAI"
+});
 
-{
-  path: '/SignInPage',
-  element: <SignInPage/>,
-  errorElement: <div>404 not found</div>
-}
-
-]);
 
 
 
